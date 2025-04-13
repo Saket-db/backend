2c58950238b892a583008bdde77199fbc5cd9d35
Saket-db <dbsaket04@gmail.com>
2025-04-13 14:24:02 +0530

    Links Updated

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.NEXT_URI,
+    origin: "https://chatify-y9kd.onrender.com",
     credentials: true,
   })
 );
